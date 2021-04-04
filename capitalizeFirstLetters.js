6edafa15b095a3eb29296c33046de11a641732a0
Solene <solene.gerard@gmail.com>
2021-04-04 20:18:11 +0200

    Revert "created first function passing test"
    This reverts commit 700c64c85275370f0b27500fc4200539ad36afdf.

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,25 +1,9 @@
 const assert = require("assert");
 
-/**function capitalizeFirstLetters(input) {
-  const splitSentence = input.toLowerCase().split("");
-  for (let i = 0; i < splitSentence.length; i++) {
-    return (splitSentence[i] =
-      splitSentence[i].charAt(0).toUpperCase() + splitSentence[i].substr(1));
-  }
-  return splitSentence.join(" ");
-}*/
-function capitalizeFirstLetters(input) {
-  let splitSentence = input.toLowerCase().split(" ");
-  for (let i = 0; i < splitSentence.length; i++) {
-    splitSentence[i] =
-      splitSentence[i].charAt(0).toUpperCase() + splitSentence[i].substring(1);
-  }
-  return splitSentence.join(" ");
-}
-assert.strictEqual(capitalizeFirstLetters("this is a test"), "This Is A Test");
+assert.strictEqual(capitalizeFirst("this is a test"), "This Is A Test");
 
-assert.strictEqual(capitalizeFirstLetters("test"), "Test");
+assert.strictEqual(capitalizeFirst("test"), "Test");
 
-assert.strictEqual(capitalizeFirstLetters("z"), "Z");
+assert.strictEqual(capitalizeFirst("z"), "Z");
 
-assert.strictEqual(capitalizeFirstLetters(""), "");
+assert.assert.strictEqual(capitalizeFirst(""), "");
