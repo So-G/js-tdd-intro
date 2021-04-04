c5c1e62cc22e19a36aaaf134aab1490eb88637c1
Solene <solene.gerard@gmail.com>
2021-04-04 20:20:49 +0200

    git tag step-1

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,9 +1,19 @@
 const assert = require("assert");
 
+function capitalizeFirst(input) {
+  let splitSentence = input.toLowerCase().split(" ");
+  for (let i = 0; i < splitSentence.length; i++) {
+    splitSentence[i] =
+      splitSentence[i].charAt(0).toUpperCase() + splitSentence[i].substring(1);
+  }
+  // Directly return the joined string
+  return splitSentence.join(" ");
+}
+
 assert.strictEqual(capitalizeFirst("this is a test"), "This Is A Test");
 
 assert.strictEqual(capitalizeFirst("test"), "Test");
 
 assert.strictEqual(capitalizeFirst("z"), "Z");
 
-assert.assert.strictEqual(capitalizeFirst(""), "");
+assert.strictEqual(capitalizeFirst(""), "");
